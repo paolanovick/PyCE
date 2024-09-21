0dd8f12291baab133e4ddb3e16e2da0e9ff52a48
Paola Novick <paola.novick@davinci.edu.ar>
2024-09-21 14:40:18 -0300

    Guardar cambios locales antes del merge

--- resources/views/home/suscripcion-detail.blade.php
+++ resources/views/home/suscripcion-detail.blade.php
@@ -1,7 +1,11 @@
-<x-layout title="Detalle de Suscripción">
+<x-layout>
+    <x-slot:title>Detalle de Suscripción</x-slot:title>
+
     <h1>Detalle de Suscripción</h1>
     <p><strong>Nombre:</strong> {{ $suscripcion->nombre }}</p>
     <p><strong>Descripción:</strong> {{ $suscripcion->descripcion }}</p>
     <p><strong>Precio:</strong> ${{ $suscripcion->precio }}</p>
-    <!-- Más detalles según tus necesidades -->
+
+    <!-- Podrías agregar un botón aquí para suscribirse -->
+    <a href="{{ route('suscripcion.index') }}" class="btn btn-primary">Volver a Suscripciones</a>
 </x-layout>
