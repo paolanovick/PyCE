db158fbd64da3c02f428fcbdfdb8b7ccf1b67120
tatilai <99092149+tatilai@users.noreply.github.com>
2024-09-21 20:02:40 -0300

    estilo de la barra de navegacion y del footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -23,14 +23,15 @@
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="https://i.pinimg.com/564x/03/22/4b/03224b0e0a3cc6261073b4cc2710ee99.jpg" alt="Logo"
-                    style="height: 60px;">
+                    style="height: 60px; border-radius:30px;">
+                    <span style="font-size: 24px; font-weight: bold; color: whitesmoke; margin-left: 10px;">Enófilo</span> 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon" style="background-color: white;"></span>
             </button>
             <div class="navbar-collapse collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="/">Home</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="/nosotros">Nosotros</a></li>
                     <li class="nav-item"><a class="nav-link" aria-current="page" href="/vinos">Vinos</a></li>
@@ -45,8 +46,16 @@
         {{ $slot }}
     </main>
 
-  <footer>
-    <p class="mb-0">Copyright &copy; Da Vinci 2024</p>
+    <footer>
+    <div class="footer-content">
+   
+        <p>Explora nuestra selección de vinos premium y descubre los mejores sabores.</p>
+       
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Enófilo. Todos los derechos reservados.</p>
+        <p><a href="#">Política de Privacidad</a> | <a href="#">Términos de Servicio</a></p>
+    </div>
 </footer>
 
 
